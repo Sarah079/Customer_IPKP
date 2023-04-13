5e9eb5975adac8733e456b609f9eb44bde96c0be
Sarah079 <102764961+Sarah079@users.noreply.github.com>
2023-04-13 13:19:28 +0200

    Home Page down

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  //logopath:
+  homeimgpath: string;
+  
+  constructor() {
+    //image location
+    this.homeimgpath = '/assets/images/Home_fam_photo.png'
+  }
 }
+
+//src\assets\images\Home_fam_photo.png
